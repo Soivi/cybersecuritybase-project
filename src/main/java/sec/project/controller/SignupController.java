8308226c464c32390de67100b0745b91940b9e5e
soivi <soivi.lauri@gmail.com>
2016-12-21 23:01:55 +0200

    list added

--- src/main/java/sec/project/controller/SignupController.java
+++ src/main/java/sec/project/controller/SignupController.java
@@ -15,13 +15,16 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import sec.project.domain.Signup;
 import sec.project.repository.SignupRepository;
 
 @Controller
+//@RestController
 public class SignupController {
 
-        @PostConstruct
+    @PostConstruct
     public void init() throws Exception {
         // this data would typically be retrieved from a database
         // this.accountDetails = new TreeMap<>();
@@ -56,10 +59,10 @@ public class SignupController {
     
     private List<Signup> signupList;
 
-    @RequestMapping("*")
-    public String defaultMapping() {
-        return "redirect:/form";
-    }
+    //@RequestMapping("*")
+    //public String defaultMapping() {
+    //    return "redirect:/form";
+    //}
 
     @RequestMapping(value = "/form", method = RequestMethod.GET)
     public String loadForm(Model model) throws Exception {
@@ -91,7 +94,36 @@ public class SignupController {
         return "form";
     }
    
+    @RequestMapping(value = "/list", method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public List<Signup> list() throws Exception {
+        this.signupList = new ArrayList<>();
+        
+        String databaseAddress = "jdbc:h2:file:./database";
+        Connection connection = DriverManager.getConnection(databaseAddress, "sa", "");
 
+
+        ResultSet resultSet = connection.createStatement().executeQuery("SELECT * FROM Signup");
+        
+        while (resultSet.next()) {
+            String id = resultSet.getString("id");
+            String name = resultSet.getString("name");
+            String address = resultSet.getString("address");
+
+            Signup signup = new Signup(name, address);
+            
+            this.signupList.add(signup);
+            //signupRepository.save(new Signup(name, address));
+            System.out.println("TÄSSÄ : " + id + "\t" + name + " " + address);
+        } 
+        
+        resultSet.close();
+        connection.close();
+
+        //model.addAttribute("signups", signupRepository.findAll());
+        return this.signupList;
+    }
+    
     @RequestMapping(value = "/form", method = RequestMethod.POST)
     public String submitForm(@RequestParam String name, @RequestParam String address) {
         signupRepository.save(new Signup(name, address));
