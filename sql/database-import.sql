b60446f5c502559b9978b66aa4590cd916287d3a
soivi <soivi.lauri@gmail.com>
2017-01-06 15:14:17 +0200

    modified import

--- sql/database-import.sql
+++ sql/database-import.sql
@@ -1,4 +1,4 @@
-INSERT INTO Signup (name, address, creditcard) VALUES ('Clank', 'Tie 1', '4532550225915829');
-INSERT INTO Signup (name, address, creditcard) VALUES ('Gex', 'Tie 2', '5557258692179716');
-INSERT INTO Signup (name, address, creditcard) VALUES ('James Pond', 'Tie 3', '6011999910910162');
-INSERT INTO Signup (name, address, creditcard) VALUES ('Sasha Nein', 'Tie 4', '349856402273838');
+INSERT INTO Signup (name, address, creditcard) VALUES ('Neo', 'Street 1', '4532550225915829');
+INSERT INTO Signup (name, address, creditcard) VALUES ('John', 'Street 2', '5557258692179716');
+INSERT INTO Signup (name, address, creditcard) VALUES ('Zed', 'Street 3', '6011999910910162');
+INSERT INTO Signup (name, address, creditcard) VALUES ('James', 'Street 4', '349856402273838');
